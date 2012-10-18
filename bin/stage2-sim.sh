c68aec6f22f1476cb4f62406d6d94f1d8b372461
Shilad Sen <shilad@gmail.com>
2012-10-18 12:11:05 -0500

    Fixed typos in stage2 sim script

--- bin/stage2-sim.sh
+++ bin/stage2-sim.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ $# -ne 4 ]; then
+if [ $# -ne 5 ]; then
     echo "usage: stage2sim.sh matrix_file matrix_transpose_file output_file max_results_per_doc total_memory_in_mb" >&2
     exit 1
 fi
 
 in=$1
-in_trans=$2
+trans=$2
 out=$3
 max_sims=$4
 mb=$5
