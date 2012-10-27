a794e6f51d131e9a32cf1e506c917d880e46eea6
Shilad Sen <shilad@gmail.com>
2012-10-27 15:30:32 -0500

    Adding matrix summarizer scripts

--- bin/summarize-matrix.sh
+++ bin/summarize-matrix.sh
@@ -9,6 +9,6 @@ matrix=$1
 lucene=$2
 mb=$3
 
-export MAVEN_OPTS="-Xmx${mb}M -ea"
+export MAVEN_OPTS="-Xmx${mb}M -ea -server"
 mvn compile &&
 mvn exec:java -D exec.mainClass="edu.macalester.wpsemsim.matrix.MatrixSummarizer" -D exec.classpathScope=runtime  -D exec.args="$matrix $lucene"
