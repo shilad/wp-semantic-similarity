5f9225aad236fe3f68564a6ac16e19b161b280ce
Shilad Sen <senxa002@ima.umn.edu>
2012-11-24 22:45:14 -0600

    Added regress scripts

---
+++ bin/regress.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "usage: $0 path/to/conf.json memory_in_mb" >&2
+    exit 1
+fi
+
+conf=$1
+mb=$2
+gold=dat/gold/combined.tab.txt
+out=/dev/null
+
+export MAVEN_OPTS="-Xmx${mb}M -ea -server"
+mvn compile
+mvn exec:java -D exec.mainClass="edu.macalester.wpsemsim.sim.RegressionFitter" -D exec.classpathScope=runtime  -D exec.args="$conf $gold $out"
