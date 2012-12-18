75b9bf5adcbe8d0cb14a0138fff77cf728ad0ca6
shilad <shilad@gmail.com>
2012-12-18 14:06:13 -0600

    Updated valid ids.

--- bin/make-matrix-ids.sh
+++ bin/make-matrix-ids.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#
+# Based roughly on https://github.com/faraday/wikiprep-esa/wiki/roadmap
+#
 
 if [ $# -ne 2 ]; then
     echo "usage: $0 path/to/conf.txt min_links" >&2
@@ -7,10 +10,12 @@ fi
 conf=$1
 minlinks=$2
 
+export LC_ALL=C
+
 ./bin/query.sh $conf main 10000000 type:normal AND ninlinks:[$minlinks TO 1000000] | 
 grep -v '\[INFO\]' |
 egrep '^[0-9][0-9]*\.[0-9][0-9]*' |
 egrep -v '^[^	]*	[^	]*	[0-9]{4}' |			# remove articles that start with a YEAR
 egrep -v '^[^	]*	[^	]*	(January|February|March|April|May|June|July|August|September|October|November|December)'  | # articles that start with a month 
 egrep -v '^[^	]*	[^	]*	[0-9][0-9]*$' |			# remove articles that are only digits
-cat >dat/valid_ids.txt
+cut -d '	' -f 2 >dat/valid_ids.txt
