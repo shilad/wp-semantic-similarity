e0d78e8f6768590f76e722f770b63482945b7b4a
shilad <shilad@gmail.com>
2013-03-26 13:18:49 -0500

    Added training script

---
+++ bin/train.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "usage: $0 jvm_mbs arg1 arg2 ..." >&2
+    exit 1
+fi
+
+mb=$1
+shift
+args=(${@// /\\ })
+
+export MAVEN_OPTS="-Xmx${mb}M -ea -server"
+mvn compile &&
+mvn exec:java -D exec.mainClass="edu.macalester.wpsemsim.sim.utils.Trainer" -D exec.classpathScope=runtime  -D exec.args="${args[*]}"
