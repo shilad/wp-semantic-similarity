79593abb0dd57bbdbbb8c473442e86bb7dc750b9
shilad <shilad@gmail.com>
2013-04-05 20:17:05 -0500

    Updating wpId filter

--- src/main/java/edu/macalester/wpsemsim/lucene/WpIdFilter.java
+++ src/main/java/edu/macalester/wpsemsim/lucene/WpIdFilter.java
@@ -3,18 +3,15 @@ package edu.macalester.wpsemsim.lucene;
 import gnu.trove.set.TIntSet;
 import gnu.trove.set.hash.TIntHashSet;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.index.AtomicReader;
 import org.apache.lucene.index.AtomicReaderContext;
 import org.apache.lucene.search.DocIdSet;
 import org.apache.lucene.search.Filter;
 import org.apache.lucene.util.Bits;
 import org.apache.lucene.util.DocIdBitSet;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.BitSet;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.logging.Logger;
 
 
@@ -25,30 +22,19 @@ import java.util.logging.Logger;
  */
 public class WpIdFilter extends Filter {
     private static final Logger LOG = Logger.getLogger(WpIdFilter.class.getName());
-    private int luceneIds[];
+    private int[] wpIds;
+//    private int luceneIds[];
+    private Map<AtomicReader, int[]> allowedLuceneIds = new HashMap<AtomicReader, int[]>();
 
     public WpIdFilter(IndexHelper helper, int wpIds[]) throws IOException {
-        LOG.info("building WpId filter for " + wpIds.length + " ids with hash " + Arrays.hashCode(wpIds));
-        TIntSet wpIdSet = new TIntHashSet(wpIds);
-        TIntSet luceneIdSet = new TIntHashSet();
-        Set<String> fields = new HashSet<String>(Arrays.asList(Page.FIELD_WPID));
-        for (int i = 0; i < helper.getReader().numDocs(); i++) {
-            Document d = helper.getReader().document(i, fields);
-            int wpId = Integer.valueOf(d.get(Page.FIELD_WPID));
-            if (wpIdSet.contains(wpId)) {
-                luceneIdSet.add(i);
-            }
-        }
-        luceneIds = luceneIdSet.toArray();
-        LOG.info("WpId filter matched " + luceneIds.length + " ids.");
-
+        this.wpIds = wpIds;
     }
 
     @Override
     public DocIdSet getDocIdSet(AtomicReaderContext context, Bits acceptDocs) throws IOException {
         BitSet bits = new BitSet();
         int i = 0;
-        for (int id : luceneIds) {
+        for (int id : getAllowedLuceneIds(context)) {
             if (acceptDocs == null || acceptDocs.get(id)) {
                 bits.set(id);
                 i++;
@@ -60,5 +46,27 @@ public class WpIdFilter extends Filter {
         }
 //        LOG.info("bit size=" + bits.size() + " set=" + n + " compared to " + luceneIds.length);
         return new DocIdBitSet(bits);
+    }
+
+    private synchronized int[] getAllowedLuceneIds(AtomicReaderContext context) throws IOException {
+        AtomicReader reader = context.reader();
+        if (allowedLuceneIds.containsKey(reader)) {
+            return allowedLuceneIds.get(reader);
+        }
+        LOG.info("building WpId filter for " + wpIds.length + " ids with hash " + Arrays.hashCode(wpIds));
+        TIntSet wpIdSet = new TIntHashSet(wpIds);
+        TIntSet luceneIdSet = new TIntHashSet();
+        Set<String> fields = new HashSet<String>(Arrays.asList(Page.FIELD_WPID));
+        for (int i = 0; i < reader.numDocs(); i++) {
+            Document d = reader.document(i, fields);
+            int wpId = Integer.valueOf(d.get(Page.FIELD_WPID));
+            if (wpIdSet.contains(wpId)) {
+                luceneIdSet.add(i);
+            }
+        }
+        int luceneIds[] = luceneIdSet.toArray();
+        LOG.info("WpId filter matched " + luceneIds.length + " ids.");
+        allowedLuceneIds.put(reader, luceneIds);
+        return luceneIds;
     }
 }
