7b8870371e63d0bdd61a29eab49ea7ed737cad94
Shilad Sen <shilad@gmail.com>
2013-04-20 15:15:36 -0500

    Added phrase analyzation script.

---
+++ bin/analyze-phrases.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "usage: $0 memory_in_mb arg1 arg2 ..." >&2
+    exit 1
+fi
+
+mb=$1
+shift
+args=(${@// /\\ })
+
+export MAVEN_OPTS="-Xmx${mb}M -ea -server"
+mvn compile &&
+mvn exec:java -D exec.mainClass="edu.macalester.wpsemsim.sim.utils.PhraseAnalyzer"  \
+              -D exec.classpathScope=runtime   \
+              -D exec.args="${args[*]}"
