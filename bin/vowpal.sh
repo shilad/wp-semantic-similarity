34b0e169140ac9b3f9c024494bf2c479cbd74b03
Shilad Sen <shilad@gmail.com>
2013-04-27 22:49:32 -0500

    Tweaks to vowpal and phrase analyzer.

--- bin/vowpal.sh
+++ bin/vowpal.sh
@@ -5,11 +5,28 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 action=$1
+shift
 
 
-if [ "$action"  == "build" ]; then
-    if [ $# -ne 4 ]; then
-        echo "usage: $0 build input.vw output_dir rank" >&2
+if [ "$action" == "encode" ]; then
+    if [ $# -lt 1 ]; then
+        echo "usage: $0 encode jvm_mbs ...." >&2
+        exit 1
+    fi
+
+    jvm_mbs=$1
+    shift
+    args=(${@// /\\ })
+
+    export MAVEN_OPTS="-Xmx${jvm_mbs}M -ea"
+    mvn compile &&
+    mvn exec:java \
+        -D exec.mainClass="edu.macalester.wpsemsim.topics.VowpalTranslator" \
+        -D exec.classpathScope=runtime  \
+        -D exec.args="encode ${args[*]}"
+elif [ "$action"  == "build" ]; then
+    if [ $# -ne 2 ]; then
+        echo "usage: $0 build vw_dir rank" >&2
         exit 1
     fi
     VW=./bin/vowpal_wabbit/vowpalwabbit/vw 
@@ -17,38 +34,48 @@ if [ "$action"  == "build" ]; then
         echo "vowpal binary expected at $VW not found." >&2
         exit 1
     fi
-    in=$2
-    out=$3
-    rank=$4
-    rm -rf $out
-    mkdir -p $out &&
+    dir=$1
+    rank=$2
+    if ! [[ -d $dir ]]; then
+        echo "vw input dir $dir not found." >&2
+        exit 1
+    fi
+    if ! [[ -f "$dir/input.vw" ]]; then
+        echo "vw input file $dir/input.vw not found." >&2
+        exit 1
+    fi
     $VW --lda $rank \
         --lda_alpha 0.1 \
         --lda_rho 0.1 \
         --lda_D 4000000 \
          --minibatch 10000 \
-        -b 22 \
         --power_t 0.5 \
         --initial_t 1 \
-        --cache_file $out/vw.cache \
         --passes 3 \
-        -p $out/articles.dat \
-        --readable_model $out/topics.dat \
-        -d dat/ensemble-sims.vw
-else
-    if [ $# -ne 4 ]; then
-        echo "usage: $0 {encode|decode} input output jvm_mbs" >&2
+        --cache_file $dir/cache.vw \
+        -d $dir/input.vw \
+        -p $dir/articles.vw \
+        --readable_model $dir/topics.vw \
+        -b 19  
+        #-b 22  
+elif [[ "$action" == "decode" ]]; then
+    if [ $# -lt 1 ]; then
+        echo "usage: $0 encode jvm_mbs ...." >&2
         exit 1
     fi
 
-    in=$2
-    out=$3
-    jvm_mbs=$4
-    
+    jvm_mbs=$1
+    shift
+    args=(${@// /\\ })
+
     export MAVEN_OPTS="-Xmx${jvm_mbs}M -ea"
     mvn compile &&
     mvn exec:java \
         -D exec.mainClass="edu.macalester.wpsemsim.topics.VowpalTranslator" \
         -D exec.classpathScope=runtime  \
-        -D exec.args="$action $in $out"
+        -D exec.args="decode ${args[*]}"
+
+else
+    echo "unknown action: $action" >&2
+    exit 1
 fi
