8e1fc61bec54163d36bd979ff873cabad556c2c2
shilad <shilad@gmail.com>
2013-05-07 20:29:36 -0500

    Implement id file.

--- src/main/java/edu/macalester/wpsemsim/sim/BuilderMain.java
+++ src/main/java/edu/macalester/wpsemsim/sim/BuilderMain.java
@@ -5,9 +5,11 @@ import edu.macalester.wpsemsim.utils.EnvConfigurator;
 import edu.macalester.wpsemsim.utils.ConfigurationFile;
 import edu.macalester.wpsemsim.utils.DefaultOptionBuilder;
 import edu.macalester.wpsemsim.utils.Env;
+import gnu.trove.list.TIntList;
 import gnu.trove.set.TIntSet;
 import gnu.trove.set.hash.TIntHashSet;
 import org.apache.commons.cli.*;
+import org.apache.commons.io.FileUtils;
 
 import java.io.*;
 import java.util.logging.Logger;
@@ -28,7 +30,7 @@ public class BuilderMain {
                 .create('o'));
         options.addOption(new DefaultOptionBuilder()
                 .hasArg()
-                .withLongOpt("buildids")
+                .withLongOpt("buildIds")
                 .withDescription("File listing ids that should be built.")
                 .create('i'));
 
@@ -44,13 +46,19 @@ public class BuilderMain {
         CommandLine cmd = conf.getCommandLine();
         conf.setShouldLoadMetrics(false);
         Env env = conf.loadEnv();
+        int idsToBuild[] = env.getMainIndex().getWpIds();
+        if (cmd.hasOption("i")) {
+            TIntSet idSet = new TIntHashSet();
+            for (String line : FileUtils.readLines(new File(cmd.getOptionValue("i")))) {
+                idSet.add(Integer.valueOf(line));
+            }
+            idsToBuild = idSet.toArray();
+        }
 
         File outputFile = new File(cmd.getOptionValue("o"));
         SimilarityMetric m = conf.loadMetric(cmd.getOptionValue("n"), true);
         PairwiseSimilarityWriter writer = new PairwiseSimilarityWriter(m, outputFile);
-        if (env.getValidIds() != null) {
-            writer.setValidIds(env.getValidIds());
-        }
-        writer.writeSims(env.getMainIndex().getWpIds(), env.getNumThreads(), env.getNumMostSimilarResults());
+        writer.setValidIds(env.getValidIds());
+        writer.writeSims(idsToBuild, env.getNumThreads(), env.getNumMostSimilarResults());
     }
 }
